b0828c767a7aca2849666a7e23eaa240b1d4e5e9
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-14 14:14:23 -0500

    props in c#

---
+++ W26Week2IntroToClasses/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26Week2IntroToClasses
+{
+    public class Product
+    {
+        // fields
+        private int id;
+        private string _name;
+        private double _price;
+
+        // constructors
+        public Product()
+        {
+            id = 0;
+            _name = "";
+            _price = 0;
+        }
+
+        public Product(int id, string name, double price)
+        {
+            this.id = id;
+            _name = name;
+            _price = price;
+        }
+
+
+        // getter / setter -> id
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+
+        // property -> _name
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
--- W26Week2IntroToClasses/Program.cs
+++ W26Week2IntroToClasses/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Product p1 = new Product();
+
+            p1.SetId(200);
+            Console.WriteLine(p1.GetId());
+
+            p1.Name = "John";
+            Console.WriteLine(p1.Name);
+
+
+            Product p2 = new Product(101, "Laptop", 1200);
+
+            Product p3 = new();
+            //var p4 = new();
+            var p5 = new Product();
+
+            //Console.WriteLine(p1._price);
+
+
         }
     }
 }
